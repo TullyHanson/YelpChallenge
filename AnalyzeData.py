bfbba69675dad1aacc0198207eee2ab166559464
TullyHanson <btullyha@uci.edu>
2016-01-29 13:28:42 -0800

    Initial commit to master

---
+++ AnalyzeData.py
@@ -0,0 +1,7 @@
+#Load in business file
+
+#Take a subset of businesses that meet category == restaurant
+
+#Of those restaurants, create a list of attributes present
+
+#Create a histogram of star count with attribute count
